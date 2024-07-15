f10cd653ee0256ab8e5af21c4949277fd5b52298
Ayush Kumar <93641660+ayushsaha1018@users.noreply.github.com>
2024-07-15 17:02:45 +0530

    solved ✅ : Sort Colors

---
+++ SortColors.cpp
@@ -0,0 +1,26 @@
+// TC : O(n), SC : O(1)
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int zero = 0;
+        int one = 0;
+        int two = 0;
+        int n = nums.size();
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i] == 0)
+                zero++;
+            else if(nums[i] == 1)
+                one++;
+            else
+                two++;
+        }
+
+        for(int i=0; i<zero; i++)
+            nums[i] = 0;
+        for(int i=zero; i<zero+one; i++)
+            nums[i] = 1;
+        for(int i=zero+one; i<n; i++)
+            nums[i] = 2;
+    }
+};
