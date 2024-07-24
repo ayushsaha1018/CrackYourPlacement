e704ea1c48e08db3b8d7abed2cfbb194ddaecb33
ayushsaha1018 <ayushsaha111@gmail.com>
2024-07-25 00:00:55 +0530

    permuations in array done

---
+++ PermutationsInArray.cpp
@@ -0,0 +1,16 @@
+// TC : O(nlogn), SC : O(1)
+
+class Solution {
+  public:
+    bool isPossible(long long a[], long long b[], int n, long long k) {
+        sort(a, a+n);
+        sort(b, b+n,  greater<long long>());
+        
+        for(int i=0; i<n; i++){
+            if(a[i] + b[i] < k)
+                return false;
+        }
+        
+        return true;
+    }
+};
