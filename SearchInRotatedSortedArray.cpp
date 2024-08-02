82f71ed9531d36271eb6db4add0a249640140524
ayushsaha1018 <ayushsaha111@gmail.com>
2024-08-02 20:01:46 +0530

    search in rotated sorted array done

---
+++ SearchInRotatedSortedArray.cpp
@@ -0,0 +1,35 @@
+// TC : O(log n), SC : O(1)
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int st = 0, en = nums.size();
+
+        while(st <= en){
+            int mid = (st + en)/2;
+
+            if(nums[mid] == target)
+                return mid;
+            
+            // left sorted
+            if(nums[st] <= nums[mid]){
+                if(nums[st] <= target && target < nums[mid]){
+                    en = mid - 1;
+                }
+                else{
+                    st = mid + 1;
+                }
+            }
+            else{
+                if(nums[mid] < target && target <= nums[en]){
+                    st = mid + 1;
+                }
+                else{
+                    en = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+};
