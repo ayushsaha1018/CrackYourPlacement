3b933279d1f20b25b240274e22cd02f136b4fba1
ayushsaha1018 <ayushsaha111@gmail.com>
2024-08-09 23:31:49 +0530

    find kth largest element in array done

---
+++ KthLargestElementInArray.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        int n = nums.size();
+        priority_queue<int> pq;
+        for(int i=0; i<n; i++){
+            pq.push(nums[i]);
+        }
+
+        for(int i=1; i<k; i++){
+            pq.pop();
+        }
+
+        return pq.top();
+    }
+};
